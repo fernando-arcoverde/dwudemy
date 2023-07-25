8d252205075d47e98a1562dfcd1f432ae780678d
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-07-25 17:21:51 -0300

    Sentanças e Bloco de Código

---
+++ secao-01-iniciando-programacao/sentenca-02.js
@@ -0,0 +1,7 @@
+console.log(1 + 3); // Mostra no console o resultado da soma.
+console.log('Meu nome é Fernando Arcoverde'); // Mostra no console uma string informando meu nome.
+console.log('Meu nome é Fernando Arcoverde e eu tenho ' + 38 + ' anos de idade.');
+
+/*
+  Esse é um comentário de múltiplas linhas.
+*/
