c3824fb22250f995a7e568fdabb7cac8df80e1d8
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-07-25 17:52:27 -0300

    Trabalhando com Dados 01

---
+++ secao-01-iniciando-programacao/dados.js
@@ -0,0 +1,12 @@
+var item = 'Caneta';
+var qtd = 10;
+var price = 6.4;
+var tax = 1.5;
+var finalPrice = price + tax;
+
+console.log(item);
+console.log(qtd);
+console.log(price);
+console.log(price + tax);
+
+console.log('Eu comprei ' + qtd + ' ' + item + 's.');
