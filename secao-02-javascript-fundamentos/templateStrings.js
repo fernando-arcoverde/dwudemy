a6762c787f25675a349b423e5c89383418e4cd10
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-07-26 12:17:55 -0300

    Template Strings

---
+++ secao-02-javascript-fundamentos/templateStrings.js
@@ -0,0 +1,13 @@
+const nome = 'Fernando';
+let idade = 38;
+const cidade = 'Recife';
+const concat = 'Olá ' + nome + ' você tem ' +idade + ' anos e mora em ' + cidade + '.';
+const template = `Olá ${nome} você tem ${idade} anos e mora em ${cidade}.`;
+
+console.log(concat);
+console.log(template);
+
+// Expressões
+
+console.log(`1 + 1 = ${1 + 1}.`);
+
