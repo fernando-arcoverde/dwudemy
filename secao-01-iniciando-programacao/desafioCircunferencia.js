04d0876e8f2fee324adcde753b75f25c3ee9c5ad
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-07-26 07:55:05 -0300

    Desafio Área Circunferência

---
+++ secao-01-iniciando-programacao/desafioCircunferencia.js
@@ -0,0 +1,9 @@
+// PI * raio * raio
+
+let raio = 10;
+const PI = 3.14;
+const totalArea = PI * raio * raio;
+const totalArea2 = Math.PI * raio * raio;
+
+console.log('O resultado da área é: ' + totalArea + 'm2');
+console.log('O resultado da área é: ' + totalArea2 + 'm2');
