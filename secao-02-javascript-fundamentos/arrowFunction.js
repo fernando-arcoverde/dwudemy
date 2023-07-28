ba0bfb3b449726b22e242f03629023ed06a03cdc
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-07-28 15:31:51 -0300

    arrow function

---
+++ secao-02-javascript-fundamentos/arrowFunction.js
@@ -0,0 +1,10 @@
+// function subtraction(num1, num2) {
+//     return num1 - num2;
+//   };
+// Transformando essa função em arrow function
+
+const subtraction = (num1, num2) => {
+    return num1 - num2;
+};
+
+console.log(subtraction(100, 30));
