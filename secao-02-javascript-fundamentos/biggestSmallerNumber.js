923b35f4ac3837a8aeb15f22cb3451c043e70195
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-02 19:06:44 -0300

    Treinando Funções - Maior e Menor número

--- secao-02-javascript-fundamentos/biggestSmallerNumber.js
+++ secao-02-javascript-fundamentos/biggestSmallerNumber.js
@@ -1,1 +1,16 @@
-function biggestSmallerNumber ()
+function biggestSmallerNumber(array) {
+    let bigger = array[0];
+    let smaller = array[0];
+
+    for (let index = 1; index < array.length; index += 1) {
+        if (array[index] > bigger) {
+            bigger = array[index];
+        } else if (array[index] < smaller) {
+            smaller = array[index];
+        };
+    };
+    
+    return `O maior número é ${bigger} e o menor número é ${smaller}.`
+};
+
+console.log(biggestSmallerNumber([3, 2, 5, 6, 9, 4]));
