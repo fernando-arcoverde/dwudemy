57f7f8d8ce5134e255f4354325c569db7f1a3cb1
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-03 22:05:25 -0300

    Criando Mega Sena - Passo 4 - Comparando os jogos

--- secao-02-javascript-fundamentos/mega-sena-array-for.js
+++ secao-02-javascript-fundamentos/mega-sena-array-for.js
@@ -1,3 +1,4 @@
+// Sorteio dos Números
 let lotteryNumbers = [];
 let firstGame = [12, 47, 48, 52, 8, 1];
 
@@ -5,5 +6,17 @@ for (let index = 0; index < 6; index += 1) {
   lotteryNumbers.push(Math.ceil(Math.random() * 60));
 }
 
-console.log(lotteryNumbers);
-console.log(firstGame);
+// Comparação do Sorteio com o meu jogo
+let numberOfHits = 0;
+
+for (let indexLottery = 0; indexLottery < lotteryNumbers.length; indexLottery += 1) {
+  for (let indexGame = 0; indexGame < firstGame.length; indexGame += 1) {
+    if (lotteryNumbers[indexLottery] === firstGame[indexGame]) {
+        numberOfHits += 1;
+    }
+  };
+};
+
+console.log(`A mega sena sorteou os seguintes números: ${lotteryNumbers}. 
+O seu jogo foi ${firstGame}. 
+Você acertou ${numberOfHits} número(s).`);
