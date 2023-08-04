98f2081b450457a10fd983af8e11a4a66a602ccc
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-03 22:52:54 -0300

    Criando Mega Sena - Passo 5 - Negando a criação de números aleatórios repetidos

--- secao-02-javascript-fundamentos/mega-sena-array-for.js
+++ secao-02-javascript-fundamentos/mega-sena-array-for.js
@@ -3,20 +3,26 @@ let lotteryNumbers = [];
 let firstGame = [12, 47, 48, 52, 8, 1];
 
 for (let index = 0; index < 6; index += 1) {
-  lotteryNumbers.push(Math.ceil(Math.random() * 60));
-}
+  let randomNumber = Math.ceil(Math.random() * 60);
+  if (!lotteryNumbers.includes(randomNumber)) {
+    lotteryNumbers.push(randomNumber);
+  };
+};
 
 // Comparação do Sorteio com o meu jogo
 let numberOfHits = 0;
+let hits = [];
 
 for (let indexLottery = 0; indexLottery < lotteryNumbers.length; indexLottery += 1) {
   for (let indexGame = 0; indexGame < firstGame.length; indexGame += 1) {
     if (lotteryNumbers[indexLottery] === firstGame[indexGame]) {
         numberOfHits += 1;
+        hits.push(lotteryNumbers[indexLottery]);
     }
   };
 };
 
 console.log(`A mega sena sorteou os seguintes números: ${lotteryNumbers}. 
 O seu jogo foi ${firstGame}. 
-Você acertou ${numberOfHits} número(s).`);
+Você acertou ${numberOfHits} número(s).
+${hits}`);
