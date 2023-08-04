eaee9b92870f1b6cb58f51bfd67ae2402eac66a9
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-03 21:23:08 -0300

    Criando Mega Sena - Passo 3 - Definindo meu Jogo

--- secao-02-javascript-fundamentos/mega-sena-array-for.js
+++ secao-02-javascript-fundamentos/mega-sena-array-for.js
@@ -1,7 +1,9 @@
 let lotteryNumbers = [];
+let firstGame = [12, 47, 48, 52, 8, 1];
 
 for (let index = 0; index < 6; index += 1) {
   lotteryNumbers.push(Math.ceil(Math.random() * 60));
 }
 
 console.log(lotteryNumbers);
+console.log(firstGame);
