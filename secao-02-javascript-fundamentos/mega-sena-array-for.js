ae4e64d5b34df878a222b2433904e42f0957030e
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-03 23:05:57 -0300

    Criando Mega Sena - Passo 6 - Ajuste passo 5

--- secao-02-javascript-fundamentos/mega-sena-array-for.js
+++ secao-02-javascript-fundamentos/mega-sena-array-for.js
@@ -6,7 +6,9 @@ for (let index = 0; index < 6; index += 1) {
   let randomNumber = Math.ceil(Math.random() * 60);
   if (!lotteryNumbers.includes(randomNumber)) {
     lotteryNumbers.push(randomNumber);
-  };
+  } else {
+    index -= 1;
+  }
 };
 
 // Comparação do Sorteio com o meu jogo
