95ba6b31f680b55fb2543915d139b828288cee6a
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-04 10:36:22 -0300

    Trabalhando com objetos - Parte 1

---
+++ secao-02-javascript-fundamentos/aula-objeto.js
@@ -0,0 +1,10 @@
+const macDonalds = {
+    bigMac: 26.60,
+    bigTasty: 39.90,
+    mcFritasMedia: 13.90,
+    topSundayChocolate: 11.90,
+    isDelicious: true,
+    city: 'Curitiba',
+    unit: 'Park Shopping Barigui',
+}
+
