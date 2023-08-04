8ceef7af5abbd1dd5fdb3caa042551c2ea4dcc8d
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-04 11:57:43 -0300

    Trabalhando com objetos - Parte 3 - Usando assign

--- secao-02-javascript-fundamentos/aula-objeto.js
+++ secao-02-javascript-fundamentos/aula-objeto.js
@@ -8,8 +8,16 @@ const macDonalds = {
     unit: 'Park Shopping Barigui',
 }
 
-macDonalds.bigMac = 78.75;
-macDonalds['bigMac'] = 178.99;
+macDonalds.bigMac = 78.75; // Dot notation
+macDonalds['bigMac'] = 178.99; // Brackets notation
+macDonalds.state = 'Paraná'
+
+console.log(Object.keys(macDonalds)); // Acessa apenas as chaves com saída de array
+console.log(Object.values(macDonalds)); // Acessa os valores das chaves com saída de array
+console.log(Object.entries(macDonalds)); // Acessa chave e valorcom saída de array
+
+const cloneMacDonalds = Object.assign({}, macDonalds); // Cria uma cópia de um objeto qualquer
 
 console.log(macDonalds);
 console.log(macDonalds.bigMac);
+console.log(cloneMacDonalds);
