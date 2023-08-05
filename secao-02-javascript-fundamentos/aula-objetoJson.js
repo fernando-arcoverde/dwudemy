4e9181bc13a69d560d48368c4babf0f347f603fa
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-05 08:07:38 -0300

    Trabalhando com objetos - Parte 4 - Criando um objetão

---
+++ secao-02-javascript-fundamentos/aula-objetoJson.js
@@ -0,0 +1,52 @@
+const menu = [
+    {
+        tipo: "sanduíche",
+        nome: "Hambúrguer Clássico",
+        preco: 10.99,
+        ingredientes: ["pão de hambúrguer", "carne bovina", "queijo cheddar", "alface", "tomate", "molho especial"],
+        calorias: 550
+    },
+    {
+        tipo: "sanduíche",
+        nome: "Sanduíche de Frango Grelhado",
+        preco: 9.99,
+        ingredientes: ["pão de hambúrguer", "peito de frango grelhado", "queijo suíço", "cebola roxa", "maionese"],
+        calorias: 420
+    },
+    {
+        tipo: "bebida",
+        nome: "Refrigerante Cola",
+        preco: 2.49,
+        ingredientes: ["água gaseificada", "açúcar", "corante", "aromatizantes"],
+        calorias: 150
+    },
+    {
+        tipo: "bebida",
+        nome: "Suco Natural de Laranja",
+        preco: 3.99,
+        ingredientes: ["laranjas frescas"],
+        calorias: 120
+    },
+    {
+        tipo: "sobremesa",
+        nome: "Sundae de Chocolate",
+        preco: 4.79,
+        ingredientes: ["sorvete de baunilha", "calda de chocolate", "chantilly", "nozes picadas"],
+        calorias: 320
+    },
+    {
+        tipo: "sobremesa",
+        nome: "Torta de Maçã",
+        preco: 5.99,
+        ingredientes: ["massa folhada", "maçãs", "açúcar", "canela"],
+        calorias: 280
+    }
+];
+
+const newItem = {
+    name: 'X-Infarto',
+    price: 57.90,
+    ingredientes: ['5 Hamburgueres', 'Queijo Cheddar', 'Queijo Catupiry', '8 Tiras de Bacon',
+    'Molho Especial', '8 Salcichas', '4 Ovos', 'Pão Bola Gigante', 'Ingredientes Suepresa'],
+    calories: 7800;
+};
