773e3e07f093a7dcd7847a047c987f83166da2ee
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-05 21:55:40 -0300

    Trabalhando com DOM4 - getElementsByTagName

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-document.getElementById("teste").innerText = "Alterando o texto através do ID e InnerText";
+// document.getElementById("teste").innerText = "Alterando o texto através do ID e InnerText";
 
-console.log(document.getElementById("teste2").innerText);
+// console.log(document.getElementById("teste2").innerText);
 
-document.getElementsByClassName("piloto-atual")[0].innerText = 'Fernando Alonso';
+// document.getElementsByClassName("piloto-atual")[0].innerText = 'Fernando Alonso';
 
-let pilotosAtuais = document.getElementsByClassName("piloto-atual");
+// let pilotosAtuais = document.getElementsByClassName("piloto-atual");
 
-for (let index = 0; index < pilotosAtuais.length; index += 1) {
-    pilotosAtuais[index].innerText = "Lewis Hamilton";
-}
+// for (let index = 0; index < pilotosAtuais.length; index += 1) {
+//     pilotosAtuais[index].innerText = "Lewis Hamilton";
+// }
+
+/* getElementsByClassName e getElementsByTagName entregam listas,
+mesmo que só tenha um elemento e deve ser acessado como posições de um array.
+Também pode ser tratado como array para realizar loops, etc.
+*/
+
+document.getElementsByTagName("span")[0].innerText = "Carlos Sanz";
