0187c388c35c07e135d2cd7bda28cd8ec89b380a
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-05 21:39:40 -0300

    Trabalhando com DOM4 - getElementsByClassName

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById("teste").innerText = "Alterando o texto através do ID e InnerText";
 
 console.log(document.getElementById("teste2").innerText);
+
+document.getElementsByClassName("piloto-atual").innerText
