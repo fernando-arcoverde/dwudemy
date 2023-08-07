6759fa6d906d3503dbc7196398d5667434b8b761
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-07 12:39:28 -0300

    Treinando parentNode

---
+++ exercicios-dom/script.js
@@ -0,0 +1,3 @@
+console.log(document.querySelector('.ingredients-item'));
+console.log(document.querySelector('.ingredients-item').parentNode);
+console.log(document.getElementsByClassName('ingredients-item'));
